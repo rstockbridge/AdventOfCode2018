06e6925326c6b1f31431c3e5f689bf779eb03442
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2018-12-01 23:45:44 -0500

    Add Day 1

--- day01/src/main/kotlin/Main.kt
+++ day01/src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
 import java.io.File
 
 fun main() {
-    val input = File(ClassLoader.getSystemResource("input.txt").file)
+    val inputAsInts = readInputFile().map(String::toInt)
 
-    // Write solution here!
+    println("Part I: the solution is ${solvePartI(inputAsInts)}.")
+    println("Part II: the solution is ${solvePartII(inputAsInts)}.")
+}
+
+fun readInputFile(): List<String> {
+    return File(ClassLoader.getSystemResource("input.txt").file).readLines()
+}
+
+fun solvePartI(frequencyChanges: List<Int>): Int {
+    return frequencyChanges.sum()
+}
+
+fun solvePartII(frequencyChanges: List<Int>): Int {
+    var sum = 0
+    val observed = mutableSetOf(sum)
+
+    outer@ while (true) {
+        for (change in frequencyChanges) {
+            sum += change
+
+            if (!observed.add(sum)) {
+                break@outer
+            }
+        }
+    }
+
+    return sum
 }
