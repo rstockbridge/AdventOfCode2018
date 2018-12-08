e60a7db88111e5be592d7f37aa1daf663aa66270
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2018-12-08 15:14:40 -0500

    Add Day 8

--- day08/src/main/kotlin/Main.kt
+++ day08/src/main/kotlin/Main.kt
@@ -1,7 +1,70 @@
 import java.io.File
 
 fun main() {
-    val input = File(ClassLoader.getSystemResource("input.txt").file)
+    val input = readInputFile()[0].split(" ").map(String::toInt)
 
-    // Write solution here!
+    println("Part I: the solution is ${solvePartI(input)}.")
+    println("Part II: the solution is ${solvePartII(input)}.")
+}
+
+fun readInputFile(): List<String> {
+    return File(ClassLoader.getSystemResource("input.txt").file).readLines()
+}
+
+fun solvePartI(input: List<Int>): Int {
+    val mutableInput = input.toMutableList()
+    var result = 0
+
+    fun processNode() {
+        val numberOfChildren = mutableInput[0]
+        mutableInput.removeAt(0)
+        val numberOfMetaDataEntries = mutableInput[0]
+        mutableInput.removeAt(0)
+
+        for (child in 0 until numberOfChildren) {
+            processNode()
+        }
+
+        for (i in 0 until numberOfMetaDataEntries) {
+            result += mutableInput[0]
+            mutableInput.removeAt(0)
+        }
+    }
+
+    processNode()
+
+    return result
+}
+
+fun solvePartII(input: List<Int>): Int {
+    val mutableInput = input.toMutableList()
+
+    fun calculateNodeValue(): Int {
+        var result = 0
+
+        val numberOfChildren = mutableInput[0]
+        mutableInput.removeAt(0)
+        val numberOfMetaDataEntries = mutableInput[0]
+        mutableInput.removeAt(0)
+
+        val childrenValues = (0..(numberOfChildren - 1)).map { calculateNodeValue() }
+
+        for (i in 0 until numberOfMetaDataEntries) {
+            val metaData = mutableInput[0]
+
+            if (numberOfChildren == 0) {
+                result += metaData
+            } else {
+                if ((metaData > 0) and (metaData <= numberOfChildren)) {
+                    result += childrenValues[metaData - 1]!!
+                }
+            }
+
+            mutableInput.removeAt(0)
+        }
+
+        return result
+    }
+
+    return calculateNodeValue()
 }
