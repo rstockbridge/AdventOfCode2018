bdcdb5b668bdf8ce7757966af49a595a2a6fd0c2
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2018-12-09 13:53:24 -0500

    Add Day 9

--- day09/src/main/kotlin/Main.kt
+++ day09/src/main/kotlin/Main.kt
@@ -1,7 +1,39 @@
-import java.io.File
+import java.util.*
 
 fun main() {
-    val input = File(ClassLoader.getSystemResource("input.txt").file)
+    println("Part I: the solution is ${solveProblem(471, 72026)}.")
+    println("Part II: the solution is ${solveProblem(471, 7202600)}.")
+}
 
-    // Write solution here!
+fun solveProblem(numberOfPlayers: Int, marbleMaxValue: Int): Long {
+    val playerScores = LongArray(numberOfPlayers)
+
+    val circle = Circle()
+    circle.addFirst(0)
+
+    for (marble in 1..marbleMaxValue) {
+        if (marble % 23 != 0) {
+            circle.rotate(2)
+            circle.addLast(marble)
+        } else {
+            circle.rotate(-7)
+            playerScores[marble % numberOfPlayers] += marble + circle.pop().toLong()
+        }
+    }
+
+    return playerScores.max()!!
+}
+
+class Circle : ArrayDeque<Int>() {
+    fun rotate(numberOfSteps: Int) {
+        if (numberOfSteps >= 0) {
+            for (i in 0 until numberOfSteps) {
+                addFirst(removeLast())
+            }
+        } else {
+            for (i in 0 until (Math.abs(numberOfSteps) - 1)) {
+                addLast(remove())
+            }
+        }
+    }
 }
