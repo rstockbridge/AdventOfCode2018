59a22741bb890c0b9afde66836eab8a90fec14f0
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2018-12-17 16:56:08 -0500

    Add Day 16

--- day16/src/main/kotlin/Main.kt
+++ day16/src/main/kotlin/Main.kt
@@ -1,7 +1,140 @@
 import java.io.File
 
 fun main() {
-    val input = File(ClassLoader.getSystemResource("input.txt").file)
+    val samples = parseInputPartI(readInputFilePartI())
+    println("Part I: the solution is ${solvePartI(samples)}.")
 
-    // Write solution here!
+    val instructions = parseInputPartII(readInputFilePartII())
+    println("Part II: the solution is ${solvePartII(samples, instructions)}.")
+}
+
+fun readInputFilePartI(): List<String> {
+    return File(ClassLoader.getSystemResource("inputPartI.txt").file).readLines()
+}
+
+fun parseInputPartI(input: List<String>): List<Sample> {
+    val beforeRegex = "Before:\\s+\\[(\\d), (\\d), (\\d), (\\d)]".toRegex()
+    val instructionRegex = "(\\d+) (\\d) (\\d) (\\d)".toRegex()
+    val afterRegex = "After:\\s+\\[(\\d), (\\d), (\\d), (\\d)]".toRegex()
+
+    return input
+            .chunked(4)
+            .map { sampleInput ->
+                val before = beforeRegex.matchEntire(sampleInput[0])!!.destructured
+                        .toList()
+                        .map(String::toInt)
+                val instruction = Instruction.fromList((instructionRegex.matchEntire(sampleInput[1])!!
+                        .destructured
+                        .toList()
+                        .map(String::toInt)))
+                val after = afterRegex.matchEntire(sampleInput[2])!!
+                        .destructured
+                        .toList()
+                        .map(String::toInt)
+                Sample(before, instruction, after)
+            }
+}
+
+fun solvePartI(samples: List<Sample>): Int {
+    return samples
+            .count { sample ->
+                Opcode.values()
+                        .count { opcode ->
+                            opcode.isConsistent(sample)
+                        } >= 3
+            }
+
+}
+
+fun readInputFilePartII(): List<String> {
+    return File(ClassLoader.getSystemResource("inputPartII.txt").file).readLines()
+}
+
+fun parseInputPartII(input: List<String>): List<Instruction> {
+    val instructionRegex = "(\\d+) (\\d) (\\d) (\\d)".toRegex()
+
+    return input.map { line ->
+        Instruction.fromList(
+                instructionRegex.matchEntire(line)!!
+                        .destructured
+                        .toList()
+                        .map(String::toInt)
+        )
+    }
+}
+
+fun solvePartII(samples: List<Sample>, instructions: List<Instruction>): Int {
+    val mapFromOpcodeIdToOpcode = matchOpcodesToOpcodeIds(calculatePossibleOpcodes(samples))
+
+    var registers = listOf(0, 0, 0, 0)
+    instructions.forEach { instruction ->
+        val opcode = mapFromOpcodeIdToOpcode[instruction.opcodeId]
+        registers = opcode!!.applyTo(registers, instruction.a, instruction.b, instruction.c)
+    }
+
+    return registers[0]
+}
+
+fun calculatePossibleOpcodes(samples: List<Sample>): Map<Int, Set<Opcode>> {
+    val result = mutableMapOf<Int, Set<Opcode>>()
+
+    for (opcodeId in 0..15) {
+        val possibleOpcodes = mutableSetOf<Opcode>()
+
+        samples
+                .filter { sample -> sample.instruction.opcodeId == opcodeId }
+                .forEach { sample ->
+                    Opcode.values()
+                            .forEach { opcode ->
+                                if (opcode.isConsistent(sample)) {
+                                    possibleOpcodes.add(opcode)
+                                }
+                            }
+                }
+
+        result[opcodeId] = possibleOpcodes
+    }
+
+    return result
+}
+
+fun matchOpcodesToOpcodeIds(possibleOpcodes: Map<Int, Set<Opcode>>): MutableMap<Int, Opcode> {
+    val mutablePossibleOpcodes = mutableMapOf<Int, MutableSet<Opcode>>()
+    possibleOpcodes.forEach { mutablePossibleOpcodes[it.key] = it.value.toMutableSet() }
+
+    val result = mutableMapOf<Int, Opcode>()
+
+    var keepGoing = true
+    while (keepGoing) {
+        for (opcodeId in 0..15) {
+            if (mutablePossibleOpcodes[opcodeId]!!.size == 1) {
+                val opcode = mutablePossibleOpcodes[opcodeId]!!.first()
+                result[opcodeId] = opcode
+
+                for (opCodeId in 0..15) {
+                    mutablePossibleOpcodes[opCodeId]!!.remove(opcode)
+                }
+            }
+        }
+
+        keepGoing = mutablePossibleOpcodes.any { it.value.isNotEmpty() }
+    }
+
+    return result
+}
+
+data class Sample(val before: List<Int>, val instruction: Instruction, val after: List<Int>)
+
+data class Instruction(val opcodeId: Int, val a: Int, val b: Int, val c: Int) {
+
+    companion object {
+        fun fromList(ints: List<Int>): Instruction {
+            return Instruction(
+                    opcodeId = ints[0],
+                    a = ints[1],
+                    b = ints[2],
+                    c = ints[3]
+            )
+        }
+    }
 }
---
+++ day16/src/main/kotlin/Opcode.kt
@@ -0,0 +1,42 @@
+enum class Opcode {
+    ADDR, ADDI,
+    MULR, MULI,
+    BANR, BANI,
+    BORR, BORI,
+    SETR, SETI,
+    GTIR, GTRI, GTRR,
+    EQIR, EQRI, EQRR;
+
+    fun applyTo(registers: List<Int>, a: Int, b: Int, c: Int): List<Int> {
+        val result = registers.toMutableList()
+
+        result[c] = when (this) {
+            ADDR -> registers[a] + registers[b]
+            ADDI -> registers[a] + b
+            MULR -> registers[a] * registers[b]
+            MULI -> registers[a] * b
+            BANR -> registers[a] and registers[b]
+            BANI -> registers[a] and b
+            BORR -> registers[a] or registers[b]
+            BORI -> registers[a] or b
+            SETR -> registers[a]
+            SETI -> a
+            GTIR -> if (a > registers[b]) 1 else 0
+            GTRI -> if (registers[a] > b) 1 else 0
+            GTRR -> if (registers[a] > registers[b]) 1 else 0
+            EQIR -> if (a == registers[b]) 1 else 0
+            EQRI -> if (registers[a] == b) 1 else 0
+            EQRR -> if (registers[a] == registers[b]) 1 else 0
+        }
+
+        return result
+    }
+
+    fun isConsistent(sample: Sample): Boolean {
+        return sample.after == this.applyTo(
+                sample.before,
+                sample.instruction.a,
+                sample.instruction.b,
+                sample.instruction.c)
+    }
+}
